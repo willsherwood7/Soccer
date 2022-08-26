27dcbdd44a83727047c30b9497680320bfc1f396
Will Sherwood <willsherwood@gmail.com>
2022-08-26 14:19:33 -0700

    Initial commit
    Hello, world

---
+++ src/Ball.java
@@ -0,0 +1,12 @@
+
+public class Ball {
+	private int size;
+	
+	public Ball () {
+		size = 4;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+}
---
+++ src/BallTester.java
@@ -0,0 +1,9 @@
+
+public class BallTester {
+	
+	public static void main(String[] args) {
+		Ball nike = new Ball();
+		System.out.println(nike.getSize());
+	}
+
+}
